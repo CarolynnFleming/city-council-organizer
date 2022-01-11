51864ff830b463b370c5c7dbaac3beb45cf1e9d8
CarolynnFleming <cfcatpink@gmail.com>
2022-01-11 15:39:05 -0800

    begin

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,7 +1,14 @@
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
+const SUPABASE_URL = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MTMzNzM5MywiZXhwIjoxOTU2OTEzMzkzfQ.5E636TNNCxoTJtTExSwOutzpIBjtS4WLgOnqfXdnvvM';
+const SUPABASE_KEY = 'https://igyvpimxugpyxqzzyuep.supabase.co';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+export async function getCityCouncilOrganizers() {
+    const response = await client
+        .from('city_council_organizer')
+        .select(`*, city_council_members`);
+    return checkError(response);
+}
 
 export async function getUser() {
     return client.auth.session();
@@ -16,7 +23,7 @@ export async function checkAuth() {
 
 export async function redirectIfLoggedIn() {
     if (await getUser()) {
-        location.replace('./other-page');
+        location.replace('./organizer');
     }
 }
 
