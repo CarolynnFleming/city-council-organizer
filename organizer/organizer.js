dcb25b738a75910bf1d8a4b0455dd5592d322962
CarolynnFleming <cfcatpink@gmail.com>
2022-01-11 17:51:15 -0800

    okay

--- organizer/organizer.js
+++ organizer/organizer.js
@@ -1,5 +1,5 @@
 import { checkAuth, getCityCouncilOrganizers, logout, deleteMember } from '../fetch-utils.js';
-
+import { renderMember } from '../render-utils.js';
 checkAuth();
 
 const logoutButton = document.getElementById('logout');
@@ -15,5 +15,31 @@ async function displayEvents(){
 
     eventsListEl.textContent = '';
 
+    for (let event of events) {
+        const eventEl = document.createElement('div');
+        const nameEl = document.createElement('h3');
+        const membersEl = document.createElement('div');
+
+        eventEl.classList.add('event');
+        nameEl.textContent = event.name;
+
+        eventEl.append(nameEl, membersEl);
+
+        for (let member of event.city_council_member) {
+            const memberEl = renderMember(member);
+
+            memberEl.addEventListener('click', async() => {
+                await deleteMember(member.id);
+                displayEvents();
+            });
+            membersEl.append(memberEl);
+        }
+        eventsListEl.append(eventEl);
+    }
 
 }
+
+window.addEventListener('load', async() => {
+
+    displayEvents();
+});
